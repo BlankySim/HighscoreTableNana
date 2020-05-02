b486efa9d53c85240378cda3f6aded5826362fc1
BlankySim <juansimon@live.com.ar>
2020-05-02 19:42:27 -0300

    Test window created

--- HighscoreTableNana/HighscoreTable.cpp
+++ HighscoreTableNana/HighscoreTable.cpp
@@ -5,3 +5,28 @@
 #include"Highscore.h"
 
 using namespace nana;
+int main()
+{
+
+
+	form window{ API::make_center(240,350) };
+	window.caption("Highscore Table");
+
+
+	label title{ window, rectangle(88,0,100,20) };
+	title.format(true);
+	title.caption("<bold blue size=10>Highscores");
+
+	label names{ window, rectangle(0,25,60,20) };
+	names.format(true);
+	names.caption("<bold blue size=10>    Names");
+
+	label scores{ window, rectangle(130,25,60,20) };
+	scores.format(true);
+	scores.caption("<bold blue size=10>Scores");
+
+
+
+	window.show();
+	exec();
+}
